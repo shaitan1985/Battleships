155e2c0d6de0bf1fe922d150bb038dab952bf9ae
Kirill Shaytanov <atray@mail.ru>
2018-02-22 14:44:00 +0300

    simple schema and comments

--- Field.py
+++ Field.py
@@ -1,11 +1,13 @@
 from random import randint
-from itertools import combinations
+from itertools import combinations_with_replacement as com
 
 class Field(object):
 
 
     def __init__(self):
+        # масив 11, потому чтов 0 индексах имена полей
         self.n = 11
+
         n = self.n
         matrix = [[0 for j in range(n)] for i in range(n)]
         matrix[0][0] = "  "
@@ -16,6 +18,7 @@ class Field(object):
 
 
     def draw_field(self, matrix=None):
+        # отрисовывает либо матрицу из self либо из параметра
         n = self.n
         matrix = self.matrix if matrix is None else matrix
         t_str = " "
@@ -24,37 +27,40 @@ class Field(object):
 
 
     def get_free_places(self):
-
-        temp_mtr = [[matrix[i][j] for i in range(n)] for j in range(n)]
+        n = self.n
+        # обработка соседних координат
+        temp_mtr = [[self.matrix[i][j] for i in range(n)] for j in range(n)]
 
         for i in
-
+        """
+         если обработку закомментить,
+         то будет все работать,
+         но корабли могут стоять вплотную
+        """
         free = [(i, j) for i in range(n) for j in range(n) if self.matrix[i][j] == 0 ]
 
         return free
 
 
     def locate_ship(self, matrix, n, level):
-        pos = randint(0, 1)
+        pos = randint(0, 1) #рандомное направление
         temp_mtr = [[matrix[i][j] for i in range(n)] for j in range(n)]
-        free = get_free_places()
+        free = get_free_places() # свободные координаты
         x = (randint(1, n), randint(1, n))
-
+        # расположение
         if x in free:
-            print(x)
-            for k in range(level):
+                for k in range(level):
                 col = x[0] if pos else x[0] + k
                 row = x[1] + k if pos else x[1]
                 print(col, row)
                 try:
+                    # Если выходим за пределы поля
                     tmp_item = temp_mtr[col][row]
                 except IndexError:
                         return False
-                print(tmp_item, '*********')
                 if tmp_item:
                     return False
                 temp_mtr[col][row] = 1
-                print(temp_mtr[col][row])
         self.draw_field(temp_mtr)
         self.matrix = temp_mtr
         self.draw_field(self.matrix)
